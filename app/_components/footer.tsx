2e9a23bb1b70def6e83aea7dcad898e625ed3992
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-08-03 10:41:11 +0900

    footerにリンクを追加

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <footer className=" bg-blue-400 p-2 text-center">
-      <p>© 2023 chakkun1121</p>
+      <a
+        href="https://chakkun1121.github.io?from=blog"
+        className="text-black visited:text-black hover:no-underline"
+        target="_blank"
+      >
+        <p>© 2023 chakkun1121</p>
+      </a>
     </footer>
   );
 }
