01a6330806f7ca435bbcb41fd1b19ce897e32a3a
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-08-31 10:30:05 +0900

    変更

--- app/lib/api/getArticleData.ts
+++ app/lib/api/getArticleData.ts
@@ -1,8 +1,15 @@
+import { postType } from "../../../@types/postType";
 import { siteUrl } from "../../layout";
 
-export async function getArticleData(title: string, page?: number) {
-  const data = await fetch(
+export async function getArticleData(
+  title: string,
+  page?: number,
+): Promise<postType> {
+  return await fetch(
     siteUrl + `/api/getPostData/${title}${page ? `?page=${page}` : ""}`,
-  ).then((res) => res.json());
-  return data;
+  )
+    .then((res) => (res.ok ? res.json() : []))
+    .catch((e) => {
+      throw new Error(e);
+    });
 }
--- app/lib/api/getRecentArticles.ts
+++ app/lib/api/getRecentArticles.ts
@@ -2,10 +2,9 @@ import { postType } from "../../../@types/postType";
 import { siteUrl } from "../../layout";
 
 export async function getRecentArticles(): Promise<postType[]> {
-  const posts: postType[] = await fetch(siteUrl + "/api/getRecentArticle/")
-    .then((res) => res.json())
-    .catch((err) => {
-      throw new Error(err);
+  return await fetch(siteUrl + `/api/getRecentArticle`)
+    .then((res) => (res.ok ? res.json() : []))
+    .catch((e) => {
+      throw new Error(e);
     });
-  return posts;
 }
