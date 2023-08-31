7b36b8b31f3df13950d5438448218bef3c98f97f
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-08-31 16:33:17 +0900

    sitemap修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,32 +1,15 @@
 import { MetadataRoute } from "next";
-import path from "path";
-import { postType } from "../@types/postType";
-import fsPromises from "fs/promises";
-import { isMultiplePageArticle } from "./lib/isMultiplePageArticle";
-import { getArticleData } from "./lib/api/getArticleData";
 import { siteUrl } from "./layout";
+import { getRecentArticles } from "./lib/api/getRecentArticles";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const currentDir = process.cwd();
-  const folders = await fsPromises.readdir(path.join(currentDir, "posts"));
-  const posts: { url: string; date: string; isShow: boolean }[] =
-    await Promise.all(
-      folders.map(
-        async (
-          title,
-        ): Promise<{ url: string; date: string; isShow: boolean }> => {
-          const data: postType = await getArticleData(title);
-          const url = `${siteUrl}posts/${title}${
-            isMultiplePageArticle(title) ? "/1" : ""
-          }/`;
-          return { url, date: data.date, isShow: data.isShow };
-        },
-      ),
-    );
-  // posts内にisShowがfalseのものがある場合は除外する
-  const filteredPosts = posts.filter((post) => post.isShow !== false);
-  return filteredPosts.map((post) => ({
-    url: post.url,
+  const recentArticles = await getRecentArticles();
+  // isShowがfalseの記事は除外する
+  recentArticles.filter((post) => post.isShow);
+  return recentArticles.map((post) => ({
+    url: post.page
+      ? `${siteUrl}article${post.link}${post.page}/`
+      : `${siteUrl}article${post.link}`,
     lastModified: post.date,
   }));
 }
