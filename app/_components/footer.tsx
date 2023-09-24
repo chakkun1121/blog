998e6a8836bac7c9013bb5aa68b389b8af904ab9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-25 08:49:47 +0900

    footerのリンクの修正

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className=" bg-blue-400 p-2 text-center">
       <a
-        href="https://chakkun1121.github.io?from=blog"
+        href="https://chakkun1121.github.io"
         className="text-black visited:text-black hover:no-underline"
         target="_blank"
       >
