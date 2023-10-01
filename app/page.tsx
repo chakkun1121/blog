37ab739466149e61a339a8569e555797af539120
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-01 23:20:51 +0000

    rssのリンクの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Posts } from "./_components/posts";
 
 export default function Page() {
@@ -5,6 +6,13 @@ export default function Page() {
     <>
       <section>
         <Posts />
+      </section>
+      <section>
+        <div className="grid auto-rows-fr grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:grid-cols-4">
+          <Link href="rss.xml" target="_blank">
+            rssフィールドでこのブログの更新を取得
+          </Link>
+        </div>
       </section>
     </>
   );
