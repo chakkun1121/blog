4e77b227bb79420a9d03e453236d4788cfe08d8c
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-05 23:31:15 +0000

    画像パスの修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { env } = require("process");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: env.BASE_PATH || "",
 };
 
 module.exports = nextConfig;
