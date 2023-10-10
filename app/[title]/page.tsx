b65a9f20077381272262902cf7517eed750866b9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-10 08:24:24 +0000

    構造化データの追加

--- app/[title]/page.tsx
+++ app/[title]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import React from "react";
-import { siteTitle } from "../layout";
+import { siteTitle, siteUrl } from "../layout";
 import { BlogLayout } from "./BlogLayout";
 import { getArticleData } from "../lib/getArticleData";
 import ReactMarkdown from "react-markdown";
@@ -10,42 +10,63 @@ import remarkGfm from "remark-gfm";
 import Link from "next/link";
 import { getAllArticleData } from "../lib/getAllArticleData";
 import { ArticleFooter } from "./ArticleFooter";
+import { Article, WithContext } from "schema-dts";
 export default async function PostPage(props: { params: { title: string } }) {
   try {
     const data = await getArticleData(props.params.title);
     // mdのheader部分を除去したファイルを準備する
     const renderFile: string = data.file.replace(/^---[\s\S]*?---/, "");
+    const jsonLd: WithContext<Article> = {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      headline: data.title,
+      description: data.description,
+      datePublished: data.date,
+      image: data.image || siteUrl + "/img/no-image.webp",
+      author: {
+        "@type": "Person",
+        name: "chakkun1121",
+        url: "https://chakkun1121.github.io/",
+      },
+    };
     return (
-      <BlogLayout>
-        <div>
-          <p>
-            投稿日:
-            {new Date(data?.date || "")?.toLocaleDateString("ja-JP") || "不明"}
-          </p>
+      <>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <BlogLayout>
           <div>
-            タグ:
-            {data?.tags?.map((tag) => (
-              <Link
-                href={"./tag/" + tag}
-                className="m-2 rounded bg-black p-2 text-white no-underline visited:text-white"
-                key={tag}
-              >
-                {tag}
-              </Link>
-            ))}
+            <p>
+              投稿日:
+              {new Date(data?.date || "")?.toLocaleDateString("ja-JP") ||
+                "不明"}
+            </p>
+            <div>
+              タグ:
+              {data?.tags?.map((tag) => (
+                <Link
+                  href={"./tag/" + tag}
+                  className="m-2 rounded bg-black p-2 text-white no-underline visited:text-white"
+                  key={tag}
+                >
+                  {tag}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-        <>
-          <h1>{data.title}</h1>
-          <ReactMarkdown
-            rehypePlugins={[rehypeRaw, rehypeSanitize] as any}
-            remarkPlugins={[remarkGfm]}
-          >
-            {renderFile}
-          </ReactMarkdown>
-        </>
-        <ArticleFooter />
-      </BlogLayout>
+          <>
+            <h1>{data.title}</h1>
+            <ReactMarkdown
+              rehypePlugins={[rehypeRaw, rehypeSanitize] as any}
+              remarkPlugins={[remarkGfm]}
+            >
+              {renderFile}
+            </ReactMarkdown>
+          </>
+          <ArticleFooter />
+        </BlogLayout>
+      </>
     );
   } catch (e) {
     notFound();
