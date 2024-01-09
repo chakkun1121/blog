686a73f5ba2ae94dd744b8d1e0a04846c6afb336
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-09 01:23:48 +0000

    sitemap更新

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,27 @@
 import { MetadataRoute } from "next";
 import { getAllArticleData } from "./lib/getAllArticleData";
 import { siteUrl } from "./meta";
+import { postType } from "../@types/postType";
+import { getAllCategoryData } from "./lib/getAllCategoryData";
+import { getCategoryAllFileData } from "./lib/getCategoryAllFileData";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const recentArticles = await getAllArticleData();
-  // isShowがfalseの記事は除外する
-  recentArticles.filter((post) => post.isShow);
+  const allArticles = [
+    ...(await getAllArticleData()),
+    ...(await getAllCategoryData().then((categories) => {
+      return Promise.all(
+        categories.map(async (category) => {
+          return await getCategoryAllFileData(category);
+        }),
+      );
+    })),
+  ].flat() as postType[]; // isShowがfalseの記事は除外する
+  allArticles.filter((post) => post.isShow);
   return [
     {
       url: `${siteUrl}/`,
     },
-    ...recentArticles.map((post) => ({
+    ...allArticles.map((post) => ({
       url: `${siteUrl}${post.link}`.replace(/\/$/, ""),
       lastModified: post.date,
     })),
