b2a2223c95e9648ef8ec6d1ae5d1994e399f60f2
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-04 16:44:24 +0900

    delete mdx

--- next.config.js
+++ next.config.js
@@ -13,6 +13,5 @@ const nextConfig = {
   },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
-const withMDX = require("@next/mdx")();
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig
