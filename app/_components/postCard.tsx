5c9565f6ddbd3e87a9d16dffcc93ffc13dc99f19
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-04 17:27:40 +0900

    画像のアスペクト比を固定

--- app/_components/postCard.tsx
+++ app/_components/postCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { postType } from "../../@types/postType";
 import { siteUrl } from "../meta";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export function PostCard({ post }: { post: postType }) {
   return (
@@ -9,11 +10,15 @@ export function PostCard({ post }: { post: postType }) {
         href={post?.link}
         className="grid gap-y-2 rounded bg-orange-100 p-2 text-black no-underline [grid-row:span_3] [grid-template-rows:subgrid] visited:text-black"
       >
-        <img
-          src={post.image || new URL(`${post.link}/og.png`, siteUrl).toString()}
-          alt={post?.title + "のサムネイル"}
-          className="aspect-video h-60 w-full rounded object-cover"
-        />
+        <AspectRatio ratio={40 / 21}>
+          <img
+            src={
+              post.image || new URL(`${post.link}/og.png`, siteUrl).toString()
+            }
+            alt={post?.title + "のサムネイル"}
+            className="rounded object-cover"
+          />
+        </AspectRatio>
         <h3 className="block text-xl">{post?.title}</h3>
         <p className="line-clamp-2 block text-M">{post?.description}</p>
       </Link>
